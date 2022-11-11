7094ab79cf20f52a573ea9164f4ee298f94e42e5
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-11 21:46:03 +0200

    Task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,55 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+let instance = "";
+const gallaryEl = document.querySelector('.gallery');
+
+gallaryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+// galleryItems.preventDefault();
+function createGallery(galleryItems) {
+    // galleryItems.preventDefault();
+    return galleryItems
+     
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+        })
+        .join('');
+   
+}
+gallaryEl.addEventListener('click', onItemsClick);
+
+function onItemsClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  console.log(event.target.dataset.source);
+
+instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`)
+  instance.show();
+  window.addEventListener('keydown', onItemClose);
+  
+}
+function onItemClose(event) {
+  
+  if (event.code !== "Escape") {
+    return;
+  }
+  instance.close();
+  window.removeEventListener('keydown', onItemClose);
+}
+
+
+
